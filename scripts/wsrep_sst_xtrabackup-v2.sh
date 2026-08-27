530ff7257cd946d037d420ab1a0c266077afc24e
Jaideep Karande <jaideep.karande@percona.com>
2026-08-27 23:32:25 +0530

    PXC-5208: CI-ONLY - allow PXB 9.1.0 for SST in Jenkins MTR
    https://perconadev.atlassian.net/browse/PXC-5208
    
    Not for merge. This commit exists only so the Jenkins MTR run can use
    PXB 9.1.0, which is currently the newest PXB able to back up this
    server; PXB 9.7.1-rc1 fails on the donor (Jenkins #891:
    "xtrabackup_checkpoints missing. xtrabackup/SST failed on DONOR").
    
    - XB_THIS_REQUIRED_VERSION 9.6.0-1 -> 9.1.0 so verify_pxb_version()
      accepts the older PXB in pxc_extra/pxb-9.6.
    - lock_ddl default REDUCED -> ON, because --lock-ddl=REDUCED is only
      available in community PXB from 9.6.0-1. 8.4 and trunk already
      default to ON via PXC-5244; the 9.7 branch predates that merge.

--- scripts/wsrep_sst_xtrabackup-v2.sh
+++ scripts/wsrep_sst_xtrabackup-v2.sh
@@ -185,7 +185,7 @@ XTRABACKUP_PREV_LTS_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.4"
 
 # Minimum PXB required versions for this node to work
 # To be able to service this version
-XB_THIS_REQUIRED_VERSION="9.6.0-1"
+XB_THIS_REQUIRED_VERSION="9.1.0"
 # To be able to service previous version
 XB_PREV_REQUIRED_VERSION="9.1.0"
 # To be able to service previous LTS version
@@ -1630,7 +1630,7 @@ function initialize_pxb_commands()
     # Starting from PXB 9.6.0-1, --lock-ddl=REDUCED is available in
     # the community version. 9.6.0-1 is the minimum required version for this
     # script to work (see XB_THIS_REQUIRED_VERSION))
-    lock_ddl="--lock-ddl=REDUCED"
+    lock_ddl="--lock-ddl=ON"
     [[ -n "$lock_ddl_opt" ]] && lock_ddl="--lock-ddl=$lock_ddl_opt"
     wsrep_log_info "Using PXB option ${lock_ddl} for SST"
 
